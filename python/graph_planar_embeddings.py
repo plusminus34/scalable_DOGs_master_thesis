7102295761cb6a22f1e2a942165ffc86f0f619b8
michaera <michael.rabinovich@inf.ethz.ch>
2018-09-25 13:11:19 +0200

    Adding sorting functions for vertices edges but not ready yet

--- python/graph_planar_embeddings.py
+++ python/graph_planar_embeddings.py
@@ -1,77 +1,81 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+
 def comb_embedding_from_graph(G):
-    # vertices should be zero based
-    A=[(n, nbrdict) for n, nbrdict in G.adjacency()]
-    pos=nx.get_node_attributes(G,'pos')
+	# vertices should be zero based
+	A=[(n, nbrdict) for n, nbrdict in G.adjacency()]
+	pos=nx.get_node_attributes(G,'pos')
 
-    comb_emb = []
-    for node in G.nodes()
-        origin = pos[node]
-        v_A = list[A[node]]
-        print 'vA before = ', vA
-        nb_pos = [pos[nb] for nb in v_A]
-        sorted(v_A, key=lambda sort_clockwise_center: clockwiseangle_and_distance(nb_pos, origin))
-        print 'vA after = ', vA
+	comb_emb = []
+	for node in G.nodes():
+		origin = pos[node]
+		v_A = list(A[node][1])
+		nb_pos = [pos[nb] for nb in v_A]
+		#print 'nb_pos = ', nb_pos
+		sorted(v_A, key=lambda sort_clockwise_center: clockwiseangle_and_distance(nb_pos, origin))
+		print 'vA after = ', vA
 
-def Faces(edges,embedding)
-   """
-   edges: is an undirected graph as a set of undirected edges
-   embedding: is a combinatorial embedding dictionary. Format: v1:[v2,v3], v2:[v1], v3:[v1] clockwise ordering of neighbors at each vertex.)
+def Faces(edges,embedding):
+	"""
+	edges: is an undirected graph as a set of undirected edges
+	embedding: is a combinatorial embedding dictionary. Format: v1:[v2,v3], v2:[v1], v3:[v1] clockwise ordering of neighbors at each vertex.)
 
-    """
+	"""
 
-    # Establish set of possible edges
-    edgeset = set()
-    for edge in edges: # edges is an undirected graph as a set of undirected edges
-        edge = list(edge)
-        edgeset |= set([(edge[0],edge[1]),(edge[1],edge[0])])
+	# Establish set of possible edges
+	edgeset = set()
+	for edge in edges: # edges is an undirected graph as a set of undirected edges
+		edge = list(edge)
+		edgeset |= set([(edge[0],edge[1]),(edge[1],edge[0])])
 
-    # Storage for face paths
-    faces = []
-    path  = []
-    for edge in edgeset:
-        path.append(edge)
-        edgeset -= set([edge])
-        break  # (Only one iteration)
+	# Storage for face paths
+	faces = []
+	path  = []
+	for edge in edgeset:
+		path.append(edge)
+		edgeset -= set([edge])
+		break  # (Only one iteration)
 
-    # Trace faces
-    while (len(edgeset) > 0):
-        neighbors = self.embedding[path[-1][-1]]
-        next_node = neighbors[(neighbors.index(path[-1][-2])+1)%(len(neighbors))]
-        tup = (path[-1][-1],next_node)
-        if tup == path[0]:
-            faces.append(path)
-            path = []
-            for edge in edgeset:
-                path.append(edge)
-                edgeset -= set([edge])
-                break  # (Only one iteration)
-        else:
-            path.append(tup)
-            edgeset -= set([tup])
-    if (len(path) != 0): faces.append(path)
-    return iter(faces)
+	# Trace faces
+	while (len(edgeset) > 0):
+		neighbors = self.embedding[path[-1][-1]]
+		next_node = neighbors[(neighbors.index(path[-1][-2])+1)%(len(neighbors))]
+		tup = (path[-1][-1],next_node)
+		if tup == path[0]:
+			faces.append(path)
+			path = []
+			for edge in edgeset:
+				path.append(edge)
+				edgeset -= set([edge])
+				break  # (Only one iteration)
+		else:
+			path.append(tup)
+			edgeset -= set([tup])
+	if (len(path) != 0): faces.append(path)
+	return iter(faces)
 
-def clockwiseangle_and_distance(point, origin):
-    refvec = [0, 1]
-    # Vector between point and the origin: v = p - o
-    vector = [point[0]-origin[0], point[1]-origin[1]]
-    # Length of vector: ||v||
-    lenvector = math.hypot(vector[0], vector[1])
-    # If length is zero there is no angle
-    if lenvector == 0:
-        return -math.pi, 0
-    # Normalize vector: v/||v||
-    normalized = [vector[0]/lenvector, vector[1]/lenvector]
-    dotprod  = normalized[0]*refvec[0] + normalized[1]*refvec[1]     # x1*x2 + y1*y2
-    diffprod = refvec[1]*normalized[0] - refvec[0]*normalized[1]     # x1*y2 - y1*x2
-    angle = math.atan2(diffprod, dotprod)
-    # Negative angles represent counter-clockwise angles so we need to subtract them 
-    # from 2*pi (360 degrees)
-    if angle < 0:
-        return 2*math.pi+angle, lenvector
-    # I return first the angle because that's the primary sorting criterium
-    # but if two vectors have the same angle then the shorter distance should come first.
-    return angle, lenvector
+def clockwiseangle_and_distance(point):#, origin):
+	print 'point = ', point
+	refvec = [0, 1]
+	# Vector between point and the origin: v = p - o
+	vector = [point[0]-origin[0], point[1]-origin[1]]
+	# Length of vector: ||v||
+	lenvector = math.hypot(vector[0], vector[1])
+	# If length is zero there is no angle
+	if lenvector == 0:
+		return -math.pi, 0
+	# Normalize vector: v/||v||
+	normalized = [vector[0]/lenvector, vector[1]/lenvector]
+	dotprod  = normalized[0]*refvec[0] + normalized[1]*refvec[1]     # x1*x2 + y1*y2
+	diffprod = refvec[1]*normalized[0] - refvec[0]*normalized[1]     # x1*y2 - y1*x2
+	angle = math.atan2(diffprod, dotprod)
+	# Negative angles represent counter-clockwise angles so we need to subtract them 
+	# from 2*pi (360 degrees)
+	if angle < 0:
+		return 2*math.pi+angle, lenvector
+	# I return first the angle because that's the primary sorting criterium
+	# but if two vectors have the same angle then the shorter distance should come first.
+	return angle, lenvector
 
 """
 In [1]: import networkx as nx
@@ -91,3 +95,30 @@ Out[7]: {1: (1, 1), 2: (2, 2)}
 
 In [8]: nx.draw(G,pos)
 """
+origin = [2,3]
+def test_functions():
+	G = nx.Graph()
+	G.add_node(0,pos=(0,0))
+	G.add_node(1,pos=(1,0))
+	G.add_node(2,pos=(0,1))
+	G.add_node(3,pos=(1,1))
+
+
+	G.add_edge(0,1)
+	G.add_edge(0,2)
+	G.add_edge(1,3)
+	G.add_edge(2,3)
+
+	pos=nx.get_node_attributes(G,'pos')
+	nx.draw(G,pos)
+
+	
+	pts = [[1,4],[2,4],[3,4],[1,3],[2,3],[3,3],[1,2],[2,2],[3,2]]
+	sorted(pts, key=lambda sort_clockwise_center: clockwiseangle_and_distance)
+
+	#comb_embedding_from_graph(G)
+	plt.show()
+
+
+if __name__ == "__main__":
+	test_functions()
