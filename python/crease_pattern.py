ce8079943879e482a9fea03105708e6b94f35ba4
michaera <michael.rabinovich@inf.ethz.ch>
2018-09-28 18:07:17 +0200

    Beautiful

--- python/crease_pattern.py
+++ python/crease_pattern.py
@@ -12,15 +12,21 @@ from shapely.ops import cascaded_union,linemerge,split,snap
 
 from drawing import *
 
-
 def crease_pattern(border_polygon, polylines):
-	# snap_points_towards_another
-	snap_curves_towards_another(border_polygon, polylines)
-	# figure after
-	border_polygon,polylines = remove_points_outside_border(border_polygon, polylines)
-	#polylines = split_polylines_to_each_other(polylines)
+	border_polygon, polylines = snap_polygons_border_to_another(border_polygon, polylines)
 	face_polygons = build_polygons(border_polygon, polylines)
 	return face_polygons
+
+def snap_polygons_border_to_another(border_polygon, polylines):
+	# snap_points_towards_another
+	snap_curves_towards_another(polylines)
+	
+	# remove points outside border and snap polygons to another
+	border_polygon,polylines = remove_points_outside_border(border_polygon, polylines)
+	polylines = split_polylines_to_each_other(polylines)
+	# snap_points_towards_another
+	snap_curves_towards_another(polylines)
+	return border_polygon, polylines
 
 def add_curve_edges_to_graph(G,vertices,coords):
 	for idx in range(coords.shape[0]-1):
@@ -135,20 +141,9 @@ def split_polylines_to_each_other(polylines):
 def snap_curves_towards_another(polylines):
 	# snap the first to second, then both to third, etc
 	for idx1 in range(0, len(polylines)-1):
-		#print 'idx1 = ', idx1
 		for idx2 in range(0, idx1+2):
-			#print 'idx2 = ', idx2
 			if idx1!=idx2:
-				#print 'here with idx1 = ', idx1, ' and idx2 = ', idx2
-				#print 'before a polygon with ', polylines[idx2]
-				#print 'snapping it with the polygon ', polylines[idx1]
 				polylines[idx2] = snap(polylines[idx2], polylines[idx1], 1e-3)
-				#print 'after a polygon with ', polylines[idx2]
-
-	#print
-	#print 'polylines after = ', polylines
-	#for p in polylines:
-		#print 'p = ', p
 	return polylines
 
 def unique_rows(a):
@@ -156,22 +151,7 @@ def unique_rows(a):
     unique_a = np.unique(a.view([('', a.dtype)]*a.shape[1]))
     return unique_a.view(a.dtype).reshape((unique_a.shape[0], a.shape[1]))
 
-"""
-
-1) Remove all points that are outside the border (call prepared geometry and then contained and/or intersects since its a polygon)
-2) Snap points towards one another (n to n-1, than n and n-1 to n-2 and more). Consider defining that epsilon in a smart way (still much smaller than most edge lengths?)
-3) Build planar graph. The vertices are all the points that are left. The edges will be defined by the curves.
-4) Create the faces. From the faces create the polygons again, and plot all in different colors.
-5) Test on various svg's.
-6) Create a grid. Subdivide the grid to add internal curves. Then intersect various polygons with the grid.
-7) Rasterize various parts and save the bookeeping information
-
-"""
-
-def test_plot_polygon_and_lines(fig_num, border_polygon, polylines):
-	fig = plt.figure(fig_num, figsize=(5,5), dpi=90)
-	ax = fig.add_subplot(111)
-
+def test_plot_polygon_and_lines(ax, border_polygon, polylines):
 	# plot border polygon
 	pol_patch = PolygonPatch(border_polygon)
 	ax.add_patch(pol_patch)
@@ -181,39 +161,16 @@ def test_plot_polygon_and_lines(fig_num, border_polygon, polylines):
 		plot_line(ax, line)
 		plot_coords(ax, line)
 
-def test_crease_pattern(border_polygon = [], polylines = []):
-	if border_polygon == []:
-		eps = 1e-2
-		#border_polygon = prep(Polygon([(0, 0), (0,1), (1, 1), (1, 0)]))
-		border_polygon = Polygon([(0, 0), (0,1), (1, 1), (1, 0)])
-		line1 = LineString([(-eps,-eps), (0.1,0), (0.5,0.5), (2,1)])
-		line2 = LineString([(0,0.5), (1.2,1.5)])
-		line3 = LineString([(0,0.4), (1 +eps,1+eps)])
-		polylines = [line1, line2, line3]
+def get_default_test_params():
+	eps = 1e-2
+	border_polygon = Polygon([(0, 0), (0,1), (1, 1), (1, 0)])
+	line1 = LineString([(-eps,-eps), (0.1,0), (0.5,0.5), (2,1)])
+	line2 = LineString([(0,0.5), (1.2,1.5)])
+	line3 = LineString([(0,0.4), (1 +eps,1+eps)])
+	polylines = [line1, line2, line3]
+	return border_polygon, polylines
 
-	# figure before remove_points_outside_border
-	test_plot_polygon_and_lines(1,border_polygon,polylines)	
-	
-	# snap_points_towards_another
-	snap_curves_towards_another(polylines)
-	# figure before remove_points_outside_border
-	test_plot_polygon_and_lines(2,border_polygon,polylines)	
-	
-	
-	# figure after
-	border_polygon,polylines = remove_points_outside_border(border_polygon, polylines)
-	polylines = split_polylines_to_each_other(polylines)
-	# snap_points_towards_another
-	snap_curves_towards_another(polylines)
-
-	test_plot_polygon_and_lines(3,border_polygon,polylines)
-	
-	face_polygons = build_polygons(border_polygon, polylines)
-	#print 'face_polygons = ', face_polygons
-	
-	fig = plt.figure(4, figsize=(5,5), dpi=90)
-	ax = fig.add_subplot(111)
-
+def plot_polygons(face_polygons, polylines, ax):
 	pol_colors = get_spaced_colors(len(face_polygons))
 	i = 0
 	for pol in face_polygons:
@@ -224,12 +181,29 @@ def test_crease_pattern(border_polygon = [], polylines = []):
 		#print 'pol_patch = ', pol_patch
 		ax.add_patch(pol_patch)
 		i += 1
+	
 	# plot lines
 	for line in polylines:
 		plot_line(ax, line)
-		plot_coords(ax, line)
+		#plot_coords(ax, line)
 
 	print 'Number of faces = ', len(face_polygons)
+
+def test_crease_pattern(border_polygon = [], polylines = []):
+	if border_polygon == []:
+		border_polygon, polylines = get_default_test_params()
+
+	f, ((ax1, ax2), (ax3, ax4)) = plt.subplots(2, 2, sharex='col', sharey='row')
+
+	# figure before remove_points_outside_border
+	test_plot_polygon_and_lines(ax1,border_polygon,polylines)	
+
+	border_polygon, polylines = snap_polygons_border_to_another(border_polygon, polylines)
+
+	test_plot_polygon_and_lines(ax2,border_polygon,polylines)
+	
+	face_polygons = build_polygons(border_polygon, polylines)
+	plot_polygons(face_polygons, polylines, ax3)
 	
 	# show all
 	plt.show()
