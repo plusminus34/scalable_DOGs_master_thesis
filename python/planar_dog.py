b1134a5c2e0a20c78d57033dde58ac865f0f6ce4
Michael Rabinovich <michael.rabinovich@inf.ethz.ch>
2018-09-29 12:46:37 +0200

    Some asserts

--- python/planar_dog.py
+++ python/planar_dog.py
@@ -2,6 +2,11 @@ from shapely.geometry import *
 from shapely.geometry.polygon import *
 
 def grid_from_boundary(border_polygon, res_x = 20, res_y = 20):
+	#MultiPoint(border_polygon).convex_hull
+	#print 'convex_hull = MultiPoint(border_poly).convex_hull.area = ', MultiPoint(border_polygon.coords).convex_hull.area
+	#print 'border_polygon.area = ', border_polygon.convex_hull.area
+	#assert abs(border_polygon.convex_hull.area-border_polygon.area
+	assert is_planar_polygon_rectangle(border_polygon), "Unsupported: Currently only supporting rectangular polygons"
 	# For now assume a rectangular border polygon
 	minx, miny, maxx, maxy = border_polygon.bounds
 	step_x = (maxx-minx)/res_x
@@ -16,3 +21,8 @@ def grid_from_boundary(border_polygon, res_x = 20, res_y = 20):
 		grid_lines.append(LineString(points))
 
 	return grid_lines
+
+def is_planar_polygon_rectangle(poly):
+	minx, miny, maxx, maxy = poly.bounds
+	rectangle = Polygon([(minx,miny),(maxx, miny),(maxx,maxy),(minx,maxy)])
+	return poly == rectangle
