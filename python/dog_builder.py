3ef76cdad49514bee54e9351c026520fa95e06bd
Michael Rabinovich <michael.rabinovich@inf.ethz.ch>
2018-10-04 10:22:14 +0200

    More

--- python/dog_builder.py
+++ python/dog_builder.py
@@ -7,22 +7,33 @@ import pyigl as igl
 
 from polygons_to_orthogonal_grids import *
 
+class Edge:
+	def __init__(self, v1, v2):
+		self.v1 = v1
+		self.v2 = v2
+
+class FoldConst:
+	def __init__(self, edge1, w1, edge2, w2):
+		self.edge1, self.w1, self.edge2, self.w2 = edge1,w1,edge2,w2
+
 def dog_builder(V_list,F_list, polylines):
 	# return global V,F, then V and F for rendering, and constraints 
-
 	# The global F just concatenates the vertices and faces list (after adding indices to faces)
+	V,F = get_global_VF(V_list, F_list)
+
+	fold_const = get_fold_consts(V_list, F_list, polylines)
 	#
 	# The constraints data is defined per vertex on the polylines.
 	# A polyline vertex is defined uniquely by its 2D coordinates (so there won't be doubles). 
 	# No need to save it but this is a good way to filter multiples of those.
 	# Each one of those is either exactly a vertex, or a point on an edge.
-	# In any case it can be represented as a point of an edge. So for every polygon that does contain this point:
+	# In any case it can be represented as a point of an edge. So for every mesh that does contain this point:
 	#	We save two indices of vertices and an appropriate weight
 	#   If we have 2 pairs of this we need one constraint, if we have 3 pairs 2, and in general for n we need n-1 (1 to 2, 2 to 3, etc)
 	#   So at the end we just need to save a list of constraints of the form:
 	#				2 points on edges being equal: (v_i1,v_i2) 0<=w_1<=1, (v_j1,v_j2) 0<=w_2<=1
 
-	# So the function should find all unique points lying on polylines, then find all polygons that touch those,
+	# So the function should find all unique points lying on polylines, then find all meshes containing those,
 	# 	then find these as points on (some) edges for each one (n such edges), and save those
 	#										 (the optimization code could use them as n-1 constraints)
 	#
@@ -36,7 +47,12 @@ def dog_builder(V_list,F_list, polylines):
 	#				And an F for triangulation that assumes this order of vertices (so for initialization build it and triangulate)
 	#
 	# In case the triangulation looks bad, do a manual one
-	V,F = get_global_VF(V_list, F_list)
+	pass
+
+def get_fold_consts(V_list, F_list, polylines):
+	# Get all polylines unique vertices
+	# Find their intersections with the meshes (build polylines from them)
+	# Locate the intersections vertices edges and weights
 	pass
 
 def get_global_VF(V_list, F_list):
