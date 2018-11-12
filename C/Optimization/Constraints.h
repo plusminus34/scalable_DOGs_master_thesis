126d3579675afcbacf8f96ce70a9821e2b6e860b
michaera <michael.rabinovich@inf.ethz.ch>
2018-11-12 10:45:20 +0100

    Adding JacobianIJV method

--- C/Optimization/Constraints.h
+++ C/Optimization/Constraints.h
@@ -8,8 +8,16 @@ public:
 	int getConstNum(){return const_n;}
 
 	virtual Eigen::VectorXd Vals(const Eigen::VectorXd& x) const = 0;
-	virtual Eigen::SparseMatrix<double> Jacobian(const Eigen::VectorXd& x) const = 0;
+	virtual std::vector<Eigen::Triplet<double> > JacobianIJV(const Eigen::VectorXd& x) const = 0;
 	virtual Eigen::SparseMatrix<double> LambdaHessian(const Eigen::VectorXd& x, const Eigen::VectorXd& lambda) const = 0;
+
+	// The user just needs to implement JacobianIJV, so other methods can more efficiently concatenate multiple constraints jacobian
+	virtual Eigen::SparseMatrix<double> Jacobian(const Eigen::VectorXd& x) const {
+		Eigen::SparseMatrix<double> Jacobian(const_n, x.rows());
+		auto IJV = JacobianIJV(x);
+		Jacobian.setFromTriplets(IJV.begin(),IJV.end());
+		return Jacobian;
+	};
 
 protected:
 	int const_n;
