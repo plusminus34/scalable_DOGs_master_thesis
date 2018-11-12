405c7622b01b51bf1c3e26722aaf40528c8ab800
michaera <michael.rabinovich@inf.ethz.ch>
2018-11-12 15:30:31 +0100

    Fixed compilation

--- C/Dog/DogLaplacian.h
+++ C/Dog/DogLaplacian.h
@@ -1,4 +1,4 @@
-#include "Quad.h"
+#include "../QuadMesh/Quad.h"
 
 Eigen::SparseMatrix<double> DOG_laplacian(const Eigen::MatrixXd& V, const Eigen::MatrixXi& Fsqr);
 Eigen::SparseMatrix<double> uniform_laplacian(const QuadTopology& quadTop);
