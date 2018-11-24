8833539c7ee83bee45e7fda40902ad666ecc8417
Michael Rabinovich <michael.rabinovich@inf.ethz.ch>
2018-11-24 13:54:50 +0100

    More maybe

--- C/Dog/Objectives/CurveInterpolationConstraintsBuilder.cpp
+++ C/Dog/Objectives/CurveInterpolationConstraintsBuilder.cpp
@@ -5,15 +5,31 @@ CurveInterpolationConstraintsBuilder::CurveInterpolationConstraintsBuilder(const
 			const double& timestep) : timestep(timestep) {
 	// Create initial curve and dest curve, save the initial frame
 	SurfaceCurve surfaceCurve; surfaceCurve.edgePoints = eS.stitched_curves[0];
-	srcCurve = new Curve(surfaceCurve.get_curve_coords(V));
+	auto initCoords = surfaceCurve.get_curve_coords(V);
+	srcCurve = new Curve(initCoords);
+	Curve::getTranslationAndFrameFromCoords(initCoords, T, F);
 	// todo save frame
 	// todo create dst curve from the curve parameters
-	// todo constructor should deallocate the curves
-	// todo interpolate should just interpolate curves and get the coordinates with the frame
-	//..std::vector<double> edge_t;
+	std::vector<double> dst_len = srcCurve->len, dst_k = srcCurve->k, dst_t = srcCurve->t;
+	for (auto& k: dst_k) k*=2;
+	for (auto& t: dst_t) t+=0.1;
+	dstCurve = new Curve(dst_len, dst_k, dst_t);
+}
+
+CurveInterpolationConstraintsBuilder::~CurveInterpolationConstraintsBuilder() {
+	delete srcCurve;
+	delete dstCurve;
 }
 
 void CurveInterpolationConstraintsBuilder::get_positional_constraints(Eigen::VectorXi& b, Eigen::VectorXd& bc) {
 	// Interpolate with timestep
+	Curve intCurve(*srcCurve,*dstCurve,timestep);
 	// Return coordinates
+	auto coords = intCurve.getCoords(T,F);
+	// TODO this gives us linear constraints and we can't use b or bc here since these are edge points
+	// So we need to indices at b and at bc
+
+	// We can generalize the previous constraints for these linear constraints (plotting but also optimization)
+	// TODO: write EdgePointConstraints? Then viewer should be able to show it and all optimization as well
+	// (Besides ARAP now anc procrustes now)
 }
--- C/Dog/Objectives/CurveInterpolationConstraintsBuilder.h
+++ C/Dog/Objectives/CurveInterpolationConstraintsBuilder.h
@@ -10,12 +10,15 @@ class CurveInterpolationConstraintsBuilder {
 public:
 	CurveInterpolationConstraintsBuilder(const Eigen::MatrixXd& V, const DogEdgeStitching& eS, 
 			const double& timestep);
+	~CurveInterpolationConstraintsBuilder();
 
 	void get_positional_constraints(Eigen::VectorXi& b, Eigen::VectorXd& bc);
 private:
 	int const_n;
-	Curve* srcCurve; 
+	Curve* srcCurve;
 	Curve* dstCurve;
+	// Rigid motion
+	Eigen::RowVector3d T; Eigen::Matrix3d F;
 	// between 0 and 1
 	const double& timestep;
 };
