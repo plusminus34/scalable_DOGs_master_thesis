662e6230ed34a911f1200bf7a58038c06a376a59
michaera <michael.rabinovich@inf.ethz.ch>
2018-12-12 09:51:41 +0100

    need a bit more

--- C/Optimization/Solvers/IpOptSolver.cpp
+++ C/Optimization/Solvers/IpOptSolver.cpp
@@ -1,5 +1,6 @@
 #include "IpOptSolver.h"
 #include "/Users/michaelrabinovich/ifopt/ifopt_core/include/ifopt/problem.h"
+//#include "/Users/michaelrabinovich/ifopt/ifopt_ipopt/include/ifopt/ipopt_solver.h"
 
 double IpOptSolver::solve_constrained(const Eigen::VectorXd& x0, Objective& obj, const Constraints& constraints, 
             Eigen::VectorXd& x) {
@@ -9,7 +10,8 @@ double IpOptSolver::solve_constrained(const Eigen::VectorXd& x0, Objective& obj,
     nlp.AddConstraintSet(std::make_shared<IpOptConstraints>(constraints));
     nlp.AddCostSet      (std::make_shared<IpOptObjective>(obj));
     nlp.PrintCurrent();
-/*
+
+    /*
     // 2. choose solver and options
     IpoptSolver ipopt;
     ipopt.SetOption("linear_solver", "mumps");
@@ -18,6 +20,6 @@ double IpOptSolver::solve_constrained(const Eigen::VectorXd& x0, Objective& obj,
     // 3 . solve
     ipopt.Solve(nlp);
     x = nlp.GetOptVariables()->GetValues();
-*/
+    */
     return 0;
 }
