f74d0dbc08b02453d4c46fbf1c6e43913332d4b1
Michael Rabinovich <michael.rabinovich@inf.ethz.ch>
2018-12-26 00:01:07 +0100

    minor

--- C/DeformationController.h
+++ C/DeformationController.h
@@ -23,6 +23,7 @@ public:
 	void update_positional_constraints(bool update_solver = true);
 	void render_positional_constraints() const {return editor->render_positional_constraints();}
 	void reset_constraints() {editor->clearHandles(); b.resize(0);bc.resize(0); reset_dog_solver();}
+	bool has_constraints() {return (b.rows() + edgePoints.size()) > 0;}
 
 	double folding_angle = 0;
 	double curve_timestep = 0;
