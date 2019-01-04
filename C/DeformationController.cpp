417d32cc93fffbc2283ce1faf6e78d29726fad49
michaera <michael.rabinovich@inf.ethz.ch>
2019-01-04 13:38:53 +0100

    More stuff and crease patterns

--- C/DeformationController.cpp
+++ C/DeformationController.cpp
@@ -14,18 +14,17 @@ void DeformationController::init_from_new_dog(Dog& dog) {
 void DeformationController::update_edited_mesh(int newEditedSubmeshI) {
 	if (globalDog->get_submesh_n() == 1) return; // Only 1 submesh (no folds)
 	if (newEditedSubmeshI == editedSubmeshI) return; // no change
-	// -1 means the global mesh
-	if (newEditedSubmeshI == -1) {
-		editedSubmeshI = -1;
-		editedSubmesh = globalDog;
-		return;
-	}
 	if ( (newEditedSubmeshI >= globalDog->get_submesh_n()) || (newEditedSubmeshI < -1 ) ) return;
 
 	if (newEditedSubmeshI != editedSubmeshI) {
 		if (editedSubmesh && (editedSubmesh!= globalDog)) delete editedSubmesh;
 		editedSubmeshI = newEditedSubmeshI;
-		editedSubmesh = globalDog->get_submesh(editedSubmeshI);
+		if (newEditedSubmeshI == -1) {
+			editedSubmesh = globalDog;
+		} else {
+			editedSubmesh = globalDog->get_submesh(editedSubmeshI);	
+		}
+		
 		dogEditor.init_from_new_dog(*editedSubmesh);
 	}
 }
