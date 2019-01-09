2b01504107d6dab1091b57cc10ce6d3d825f9c03
michaera <michael.rabinovich@inf.ethz.ch>
2019-01-09 15:00:42 +0100

    Besser

--- C/Folding/MVFolds.cpp
+++ C/Folding/MVFolds.cpp
@@ -3,14 +3,21 @@
 using namespace std;
 
 MountainValleyFold::MountainValleyFold(const Dog& dog, int curve_idx, int edge_idx, bool is_mountain) : is_mountain(is_mountain) {
+	const Eigen::MatrixXd& V = dog.getV();
 	auto eS = dog.getEdgeStitching(); const vector<EdgePoint>& foldingCurve = eS.stitched_curves[curve_idx];	
 	ep = foldingCurve[edge_idx]; ep_b = foldingCurve[edge_idx-1]; ep_b = foldingCurve[edge_idx+1];
 	dog.get_2_inner_vertices_from_edge(ep.edge,v1,v2);
 
-	Eigen::RowVector3d edgeCoords = ep.getPositionInMesh(dog.getV());
-	len1 = (edgeCoords-dog.getV().row(v1)).norm();
-	len2 = (edgeCoords-dog.getV().row(v2)).norm();
 
+
+	Eigen::RowVector3d p0 = ep.getPositionInMesh(V);
+	len1 = (p0-V.row(v1)).norm();
+	len2 = (p0-V.row(v2)).norm();
+
+	Eigen::RowVector3d pf = ep_f.getPositionInMesh(V), pb = ep_b.getPositionInMesh(V);
+	Eigen::RowVector3d t = ((pf-p0).normalized()-(pb-p0).normalized()).normalized();
+	// The angle the curve folds makes with the folded edge
+	curve_tangents_angle = acos(t.dot( (p0-V.row(v1)).normalized() ));
 }
 void MVFoldingConstraintsBuilder::add_fold(const Dog& dog, int curve_idx, int edge_idx, bool is_mountain) {
 	MountainValleyFold mvF(dog, curve_idx, edge_idx, is_mountain);
