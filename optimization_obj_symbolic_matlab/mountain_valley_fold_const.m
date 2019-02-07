8f4493a5f1575bf70ea3a27db4833fc705705169
Michael Rabinovich <michael.rabinovich@inf.ethz.ch>
2019-02-07 15:04:32 +0100

    Fixed M?V folds

--- optimization_obj_symbolic_matlab/mountain_valley_fold_const.m
+++ optimization_obj_symbolic_matlab/mountain_valley_fold_const.m
@@ -1,12 +1,14 @@
-% We have 3 edge points (for oscullating plane) and 2 normal points to
-% check their angle with their binormals
+% We have 3 edge points (for oscullating plane), ep_0,ep_b,ep_f
+% We also have two additional "inner folded points" for folding. One of
+% them might be the vertex 'v' but it doesn't matter which
 % Each edge point is defined by two vertices and a fixed parameter t
-% The central edge point is p0
-% We then need one "inner point" on the fold, v1.
+% The central edge point is at t*ep_0_v1 + (1-t)ep_0_v2
+% The vertex 'v' is the one we fold
 % This function is the dot product of v1-p0 with the binormal B
 % So the condition that its positive means its a valley fold
 % To have a mountain constraint, flip the sign of the function
 % (value, gradient and hessian)
+
 ep_b_v1_x = sym('ep_b_v1_x', 'real');
 ep_b_v1_y = sym('ep_b_v1_y', 'real');
 ep_b_v1_z= sym('ep_b_v1_z', 'real');
@@ -21,20 +23,47 @@ ep_b_t = sym('ep_b_t', 'real'); assume(ep_b_t > 0);
 
 ep_b = ep_b_v1*ep_b_t+(1-ep_b_t)*ep_b_v2;
 
-% Folded points
-fold_v_x = sym('fold_v_x', 'real');
-fold_v_y = sym('fold_v_y', 'real');
-fold_v_z = sym('fold_v_z', 'real');
-fold_v = [fold_v_x,fold_v_y,fold_v_z];
+ep_f_v1_x = sym('ep_f_v1_x', 'real');
+ep_f_v1_y = sym('ep_f_v1_y', 'real');
+ep_f_v1_z= sym('ep_f_v1_z', 'real');
+ep_f_v1 = [ep_f_v1_x,ep_f_v1_y,ep_f_v1_z];
+
+ep_f_v2_x = sym('ep_f_v2_x', 'real');
+ep_f_v2_y = sym('ep_f_v2_y', 'real');
+ep_f_v2_z= sym('ep_f_v2_z', 'real');
+ep_f_v2 = [ep_f_v2_x,ep_f_v2_y,ep_f_v2_z];
+
+ep_f_t = sym('ep_f_t', 'real'); assume(ep_f_t > 0);
+ep_f = ep_f_v1*ep_f_t+(1-ep_f_t)*ep_f_v2;
+
+% Fold edge point
+ep_0_v1_x = sym('ep_0_v1_x', 'real');
+ep_0_v1_y = sym('ep_0_v1_y', 'real');
+ep_0_v1_z = sym('ep_0_v1_z', 'real');
+ep_0_v1 = [ep_0_v1_x,ep_0_v1_y,ep_0_v1_z];
+
+ep_0_v2_x = sym('ep_0_v2_x', 'real');
+ep_0_v2_y = sym('ep_0_v2_y', 'real');
+ep_0_v2_z = sym('ep_0_v2_z', 'real');
+ep_0_v2 = [ep_0_v2_x,ep_0_v2_y,ep_0_v2_z];
+
+ep_0_t = sym('ep_0_t', 'real'); assume(ep_0_t > 0);
+ep_0 = ep_0_t*ep_0_v1+(1-ep_0_t)*ep_0_v2;
+
+% Folded point
+folded_v_x = sym('folded_v_x', 'real');
+folded_v_y = sym('folded_v_y', 'real');
+folded_v_z = sym('folded_v_z', 'real');
+folded_v = [folded_v_x,folded_v_y,folded_v_z];
 
 % If there's an isometry energy these should be around the same length
-edge = fold_v-ep_0;
+edge = folded_v-ep_0;
 
 % curve binormal vec, not normalized because it's the same from both sides
 B = simplify(cross(ep_0-ep_b,ep_f-ep_0));
 const = dot(B,edge);
 
-vars = [ep_b_v1_x, ep_b_v1_y, ep_b_v1_z, ep_b_v2_x, ep_b_v2_y, ep_b_v2_z, ep_f_v1_x, ep_f_v1_y, ep_f_v1_z, ep_f_v2_x, ep_f_v2_y, ep_f_v2_z, fold_v_x,fold_v_y,fold_v_z];
+vars = [ep_0_v1_x,ep_0_v1_y,ep_0_v1_z,ep_0_v2_x,ep_0_v2_y,ep_0_v2_z,ep_b_v1_x, ep_b_v1_y, ep_b_v1_z, ep_b_v2_x, ep_b_v2_y, ep_b_v2_z, ep_f_v1_x, ep_f_v1_y, ep_f_v1_z, ep_f_v2_x, ep_f_v2_y, ep_f_v2_z,folded_v_x,folded_v_y,folded_v_z];
 
 ccode(const ,'file','V_fold_vals');
 ccode(gradient(const,vars),'file','V_fold_G');
