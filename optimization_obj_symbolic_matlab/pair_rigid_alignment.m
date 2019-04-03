335b8bd1ccf2d1d065974ca52540a5d684371d8a
Michael Rabinovich <michael.rabinovich@inf.ethz.ch>
2019-04-03 11:14:32 +0200

    Added more symbolic math code

---
+++ optimization_obj_symbolic_matlab/pair_rigid_alignment.m
@@ -0,0 +1,24 @@
+p1_x = sym('p1_x', 'real');
+p1_y = sym('p1_y', 'real');
+p1_z = sym('p1_z', 'real');
+
+p1 = [p1_x,p1_y,p1_z];
+
+p2_x = sym('p2_x', 'real');
+p2_y = sym('p2_y', 'real');
+p2_z = sym('p2_z', 'real');
+
+p2 = [p2_x,p2_y,p2_z];
+
+syms r11 r12 r13 r21 r22 r23 r31 r32 r33;
+R = [r11,r12,r13;r21,r22,r23;r31,r32,r33];
+syms t1 t2 t3
+T = [t1,t2,t3];
+
+% Equal up to rigid motion
+const = T+p1*R-p2;
+E = sum(const.^2);
+
+ccode(E,'file','pair_rigid_alignment_E');
+ccode(gradient(E),'file','pair_rigid_alignment_G');
+ccode(hessian(E),'file','pair_rigid_alignment_H');
