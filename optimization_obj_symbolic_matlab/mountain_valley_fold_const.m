4923e4130fec4ecf241c9afe824e63e41f737b12
Michael Rabinovich <michael.rabinovich@inf.ethz.ch>
2019-04-24 10:38:52 +0200

    MV folds

--- optimization_obj_symbolic_matlab/mountain_valley_fold_const.m
+++ optimization_obj_symbolic_matlab/mountain_valley_fold_const.m
@@ -47,19 +47,36 @@ v2_y = sym('v2_y', 'real');
 v2_z = sym('v2_z', 'real');
 v2 = [v2_x,v2_y,v2_z];
 
+% Folded points
+w1_x = sym('w1_x', 'real');
+w1_y = sym('w1_y', 'real');
+w1_z = sym('w1_z', 'real');
+w1 = [w1_x,w1_y,w1_z];
+
+w2_x = sym('w2_x', 'real');
+w2_y = sym('w2_y', 'real');
+w2_z = sym('w2_z', 'real');
+w2 = [w2_x,w2_y,w2_z];
+
 ep_0_t = sym('ep_0_t', 'real'); assume(ep_0_t > 0);
 ep_0 = ep_0_t*v1+(1-ep_0_t)*v2;
 
-e1 = v1-v2;
-B = simplify(cross(ep_0-ep_b,ep_f-ep_0));
+l1 = sym('l1','real');
+l2 = sym('l2','real');
+curve_t = (l2*(ep_0-ep_b)+l1*(ep_f-ep_0))/(l1*l2);
 
-alpha = sym('alpha', 'real'); assume(ep_0_t > 0);
+t1 = v1-v2;
+t2 = w1-w2;
+
+tangent_tangent_plane_dot = dot(t1, cross(curve_t,t2));
+
+%alpha = sym('alpha', 'real'); assume(ep_0_t > 0);
 
 % We want <e1,B> = - <e2,B> or <e1,B>+<e2,B> = 0
 %curve_fold_const = simplify(tanh(alpha*dot(e1,B)) + tanh(alpha*dot(e2,B)));
-mv_const = 0.5 + 0.5*tanh(alpha*dot(e1,B));
+mv_const = 0.5 + 0.5*tanh(1000*tangent_tangent_plane_dot);
 
-vars = [ep_b_v1_x, ep_b_v1_y, ep_b_v1_z, ep_b_v2_x, ep_b_v2_y, ep_b_v2_z, ep_f_v1_x, ep_f_v1_y, ep_f_v1_z, ep_f_v2_x, ep_f_v2_y, ep_f_v2_z, v1_x, v1_y, v1_z, v2_x, v2_y, v2_z];
+vars = [ep_b_v1_x, ep_b_v1_y, ep_b_v1_z, ep_b_v2_x, ep_b_v2_y, ep_b_v2_z, ep_f_v1_x, ep_f_v1_y, ep_f_v1_z, ep_f_v2_x, ep_f_v2_y, ep_f_v2_z, v1_x, v1_y, v1_z, v2_x, v2_y, v2_z, w1_x, w1_y, w1_z, w2_x, w2_y, w2_z];
 
 ccode(mv_const  ,'file','MV_fold_const');
 ccode(gradient(mv_const,vars),'file','MV_fold_G');
